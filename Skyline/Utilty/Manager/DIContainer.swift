420713a8cf9ece8a198d08b42e6c27937f7b7e13
Basmala239 <abuziedbasmala@gmail.com>
2026-06-24 15:46:23 +0300

    add get current location with permation

---
+++ Skyline/Utilty/Manager/DIContainer.swift
@@ -0,0 +1,17 @@
+//
+//  DIContainer.swift
+//  Skyline
+//
+//  Created by بسمله ابوزيد احمد on 24/06/2026.
+//
+
+import Foundation
+
+class DIContainer {
+    
+    static let shared = DIContainer()
+    
+    private init() {}
+    
+     
+}
